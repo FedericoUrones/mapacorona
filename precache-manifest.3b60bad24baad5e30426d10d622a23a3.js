7e105e807c6d6a05cc3ddbf6095d6e662e0bafc4
CTIMOVIL\EXA92832 <federico.urones@claro.com.ar>
2020-03-25 16:45:16 -0300

    Updates

---
+++ precache-manifest.3b60bad24baad5e30426d10d622a23a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f37c4813115770f7bb0cf5fb5b87fc8",
+    "url": "/mapacorona/index.html"
+  },
+  {
+    "revision": "cc35fd23a681b211769d",
+    "url": "/mapacorona/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b2f314904deb6de232d3",
+    "url": "/mapacorona/static/css/main.e0c702b4.chunk.css"
+  },
+  {
+    "revision": "cc35fd23a681b211769d",
+    "url": "/mapacorona/static/js/2.8444d17f.chunk.js"
+  },
+  {
+    "revision": "c90666e6d86345db0af07949470db633",
+    "url": "/mapacorona/static/js/2.8444d17f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f314904deb6de232d3",
+    "url": "/mapacorona/static/js/main.55891665.chunk.js"
+  },
+  {
+    "revision": "3971ae8f08050efd31b6",
+    "url": "/mapacorona/static/js/runtime-main.34cdfb89.js"
+  }
+]);
